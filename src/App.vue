e21fddf5aae71fbc9eaf66e779f4db2c698113bb
P4P1_ <ndoyepapy@live.fr>
2021-11-15 10:51:32 +0000

    feat: add windicss to the project

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>Hello from App</h1>
 </template>
---
+++ windi.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite-plugin-windicss";
+
+export default defineConfig({
+  theme: {
+    fontFamily: {
+      sans: ["Kumbh Sans", "sans-serif"],
+    },
+  },
+});

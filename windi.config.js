8204112f13e4225f557842ed9878f683ad298d2c
P4P1_ <ndoyepapy@live.fr>
2021-11-15 12:13:19 +0000

    feat: add custom colors to windicss config

--- windi.config.js
+++ windi.config.js
@@ -5,5 +5,18 @@ export default defineConfig({
     fontFamily: {
       sans: ["Kumbh Sans", "sans-serif"],
     },
+    colors: {
+      transparent: "transparent",
+      primary: {
+        200: "hsl(25, 100%, 94%)",
+        800: "hsl(26, 100%, 55%)",
+      },
+      neutral: {
+        200: "hsl(223, 64%, 98%)",
+        400: "hsl(220, 14%, 75%)",
+        600: "hsl(219, 9%, 45%)",
+        800: "hsl(220, 13%, 13%)",
+      },
+    },
   },
 });

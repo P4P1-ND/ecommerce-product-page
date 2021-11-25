21a041df15cebf004b4440aff83b9ea47d3b6206
P4P1_ <ndoyepapy@live.fr>
2021-11-25 13:20:12 +0000

    fix: change button variant to 'icon'

--- src/components/atoms/buttons/CloseButton.vue
+++ src/components/atoms/buttons/CloseButton.vue
@@ -4,7 +4,7 @@ import IconClose from "../icons/IconClose.vue";
 </script>
 
 <template>
-  <AppButton variant="flat" class="group" aria-label="Close Button">
+  <AppButton variant="icon" class="group p-0" aria-label="Close Button">
     <IconClose class="group-hover:text-neutral-800 group-focus:text-neutral-800" />
   </AppButton>
 </template>

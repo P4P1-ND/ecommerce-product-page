d448614608fccfa7d8d7f63f7b1469cdb26b2923
P4P1_ <ndoyepapy@live.fr>
2021-11-26 12:44:10 +0000

    feat: close the cart menu when clicking away

--- src/components/molecules/CartMenu.vue
+++ src/components/molecules/CartMenu.vue
@@ -1,9 +1,34 @@
 <script setup>
 import ItemCheckout from "./ItemCheckout.vue";
+import { inject } from "vue";
+
+const store = inject("store");
+
+const {
+  actions: { handleCartMenu },
+} = store.cart;
+
+// https://stackoverflow.com/questions/36170425/detect-click-outside-element#answer-64698630
+const vClickOutside = {
+  beforeMount: (el, binding) => {
+    el.clickOutsideEvent = (event) => {
+      // here I check that click was outside the el and his children
+      if (!(el == event.target || el.contains(event.target))) {
+        // and if it did, call method provided in attribute value
+        binding.value();
+      }
+    };
+    document.addEventListener("click", el.clickOutsideEvent);
+  },
+  unmounted: (el) => {
+    document.removeEventListener("click", el.clickOutsideEvent);
+  },
+};
 </script>
 
 <template>
   <div
+    v-click-outside="handleCartMenu"
     class="
       absolute
       top-full
@@ -17,6 +42,8 @@ import ItemCheckout from "./ItemCheckout.vue";
       z-5
       md:top-2/3
       xl:-right-10
+      focus:(border-2
+      border-primary-800)
     "
   >
     <header class="p-5 border-b-2 border-neutral-200">

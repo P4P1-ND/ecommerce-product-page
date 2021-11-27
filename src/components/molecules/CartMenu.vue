e2fa6bd2f1b1ed4279183c4dacf04d5d74549da1
Papy NDOYE <ndoyepapy@gmail.com>
2021-11-27 23:59:35 +0000

    refac: move AppAnimation to CartMenu component

--- src/components/molecules/CartMenu.vue
+++ src/components/molecules/CartMenu.vue
@@ -1,11 +1,13 @@
 <script setup>
 import ItemCheckout from "./ItemCheckout.vue";
+import AppAnimation from "../atoms/AppAnimation.vue";
 import { inject } from "vue";
 
 const store = inject("store");
 
 const {
   actions: { handleCartMenu },
+  openCartMenu,
 } = store.cart;
 
 // https://stackoverflow.com/questions/36170425/detect-click-outside-element#answer-64698630
@@ -27,28 +29,31 @@ const vClickOutside = {
 </script>
 
 <template>
-  <div
-    v-click-outside="handleCartMenu"
-    class="
-      absolute
-      top-full
-      inset-x-3
-      max-w-[360px]
-      mt-3
-      ml-auto
-      bg-white
-      rounded-lg
-      shadow-cart
-      z-5
-      md:top-2/3
-      xl:-right-10
-      focus:(border-2
-      border-primary-800)
-    "
-  >
-    <header class="p-5 border-b-2 border-neutral-200">
-      <h2 class="text-black font-bold">Cart</h2>
-    </header>
-    <ItemCheckout />
-  </div>
+  <AppAnimation variant="fade-vertical">
+    <div
+      v-if="openCartMenu"
+      v-click-outside="handleCartMenu"
+      class="
+        absolute
+        top-full
+        inset-x-3
+        max-w-[360px]
+        mt-3
+        ml-auto
+        bg-white
+        rounded-lg
+        shadow-cart
+        z-5
+        md:top-2/3
+        xl:-right-10
+        focus:(border-2
+        border-primary-800)
+      "
+    >
+      <header class="p-5 border-b-2 border-neutral-200">
+        <h2 class="text-black font-bold">Cart</h2>
+      </header>
+      <ItemCheckout />
+    </div>
+  </AppAnimation>
 </template>

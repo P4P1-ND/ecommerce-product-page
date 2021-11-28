bbd4f83a7c6ed2a99c8e93ef22ca4d9787683bf0
Papy NDOYE <ndoyepapy@gmail.com>
2021-11-28 13:49:42 +0000

    feat(access): add aria-label to carousel buttons

--- src/components/atoms/buttons/IconNextButton.vue
+++ src/components/atoms/buttons/IconNextButton.vue
@@ -4,7 +4,11 @@ import IconNext from "../icons/IconNext.vue";
 </script>
 
 <template>
-  <AppButton variant="icon" class="w-10 h-10 bg-white rounded-full group">
+  <AppButton
+    variant="icon"
+    class="w-10 h-10 bg-white rounded-full group"
+    aria-label="Scroll to next item"
+  >
     <IconNext class="group-focus:text-primary-800 group-hover:text-primary-800 duration-300" />
   </AppButton>
 </template>
--- src/components/atoms/buttons/IconPrevButton.vue
+++ src/components/atoms/buttons/IconPrevButton.vue
@@ -4,7 +4,11 @@ import IconPrevious from "../icons/IconPrevious.vue";
 </script>
 
 <template>
-  <AppButton variant="icon" class="w-10 h-10 bg-white rounded-full group">
+  <AppButton
+    variant="icon"
+    class="w-10 h-10 bg-white rounded-full group"
+    aria-label="Scroll to previous item"
+  >
     <IconPrevious
       class="mr-0.5 group-focus:text-primary-800 group-hover:text-primary-800 duration-300"
     />

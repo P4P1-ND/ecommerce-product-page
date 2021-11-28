14d90621cd5acff4835f0cded329e90c8ff62005
Papy NDOYE <ndoyepapy@gmail.com>
2021-11-28 13:51:16 +0000

    refac: add variant to closeButton

--- src/components/atoms/buttons/CloseButton.vue
+++ src/components/atoms/buttons/CloseButton.vue
@@ -1,10 +1,27 @@
 <script setup>
+import { computed } from "vue";
 import AppButton from "./AppButton.vue";
 import IconClose from "../icons/IconClose.vue";
+
+const props = defineProps({
+  variantClose: {
+    type: String,
+    required: true,
+    validator: (val) => ["primary", "secondary"].includes(val),
+  },
+});
+
+const computedStyles = computed(() => {
+  const primaryStyles = "text-white group-hover:text-primary-800 group-focus:text-primary-800";
+  const secondaryStyles =
+    "text-neutral-600 group-hover:text-neutral-800 group-focus:text-neutral-800";
+
+  return props.variantClose === "primary" ? primaryStyles : secondaryStyles;
+});
 </script>
 
 <template>
   <AppButton variant="icon" class="group p-0" aria-label="Close Button">
-    <IconClose class="group-hover:text-neutral-800 group-focus:text-neutral-800" />
+    <IconClose :class="computedStyles" />
   </AppButton>
 </template>

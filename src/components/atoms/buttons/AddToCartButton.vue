6b3924a24f0f504d8901a54111ab099ff4ae760d
Papy NDOYE <ndoyepapy@gmail.com>
2021-12-06 18:11:40 +0000

    refac: use 'disabled' attribute in AddToCartButton

--- src/components/atoms/buttons/AddToCartButton.vue
+++ src/components/atoms/buttons/AddToCartButton.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { inject, computed } from "vue";
+import { inject } from "vue";
 import AppButton from "./AppButton.vue";
 import IconCart from "../icons/IconCart.vue";
 
@@ -7,16 +7,22 @@ const store = inject("store");
 const {
   actions: { addProductToCart, isAmountEmpty },
 } = store.cart;
-
-const computedStyles = computed(() => {
-  return isAmountEmpty()
-    ? "cursor-not-allowed brightness-70"
-    : "shadow-button hover:brightness-125 focus:brightness-125";
-});
 </script>
 
 <template>
-  <AppButton @click="addProductToCart" :class="computedStyles" class="bg-primary-800 filter">
+  <AppButton
+    @click="addProductToCart"
+    class="
+      bg-primary-800
+      shadow-button
+      hover:opacity-60
+      focus:opacity-60
+      disabled:(cursor-not-allowed
+      filter
+      brightness-70)
+    "
+    :disabled="isAmountEmpty()"
+  >
     <IconCart color="white" />
     <span class="text-white text-base font-bold">Add to cart</span>
   </AppButton>

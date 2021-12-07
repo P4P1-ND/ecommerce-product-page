93b1ccfa3905181de41235bd4e2beee89161a933
Papy NDOYE <ndoyepapy@gmail.com>
2021-12-07 13:29:44 +0000

    feat: implement the features of the lightbox

--- src/components/molecules/backdrops/LightboxBackdrop.vue
+++ src/components/molecules/backdrops/LightboxBackdrop.vue
@@ -1,12 +1,47 @@
 <script setup>
-import { inject } from "vue";
+import { watchEffect, ref, inject } from "vue";
 import CloseButton from "../../atoms/buttons/CloseButton.vue";
 import AppBackdrop from "../../atoms/AppBackdrop.vue";
 import AppAnimation from "../../atoms/AppAnimation.vue";
 import AppButton from "../../atoms/buttons/AppButton.vue";
+import IconPrevButton from "../../atoms/buttons/IconPrevButton.vue";
+import IconNextButton from "../../atoms/buttons/IconNextButton.vue";
 
 const store = inject("store");
-const { imgList, mainImg } = store.product;
+const {
+  imgList,
+  mainImg,
+  actions: { changeMainImg },
+} = store.product;
+
+const activeThumbnail = ref(null);
+
+watchEffect(() => {
+  if (activeThumbnail.value) {
+    const index = imgList.value.findIndex((el) => el === mainImg.value);
+    activeThumbnail.value.style.transform = `translateX(calc(${index} * (100% + 16px)))`;
+  }
+});
+
+const nextItem = () => {
+  const currentImageNumber = imgList.value.findIndex((el) => el === mainImg.value) + 1;
+  let nextImageNumber;
+
+  if (currentImageNumber === imgList.value.length) nextImageNumber = 1;
+  else nextImageNumber = currentImageNumber + 1;
+
+  changeMainImg(`image-product-${nextImageNumber}`);
+};
+
+const prevItem = () => {
+  const currentImageNumber = imgList.value.findIndex((el) => el === mainImg.value) + 1;
+  let prevImageNumber;
+
+  if (currentImageNumber === 1) prevImageNumber = imgList.value.length;
+  else prevImageNumber = currentImageNumber - 1;
+
+  changeMainImg(`image-product-${prevImageNumber}`);
+};
 </script>
 
 <template>
@@ -26,25 +61,34 @@ const { imgList, mainImg } = store.product;
         "
       >
         <CloseButton variantClose="primary" @click="close" class="justify-self-end" />
-        <AppButton variant="flat" class="rounded-3xl overflow-hidden">
-          <picture>
-            <source type="image/avif" :srcset="`/img/products/${mainImg}.avif`" />
-            <img
-              width="400"
-              height="400"
-              class="w-full object-cover"
-              :src="`/img/products/${mainImg}.jpg`"
-              alt="Main Product Image"
-            />
-          </picture>
-        </AppButton>
+
+        <div class="h-100 relative">
+          <AppButton tabindex="-1" variant="flat" class="rounded-3xl overflow-hidden">
+            <picture>
+              <source type="image/avif" :srcset="`/img/products/${mainImg}.avif`" />
+              <img
+                width="400"
+                height="400"
+                class="w-full object-cover"
+                :src="`/img/products/${mainImg}.jpg`"
+                alt="Main Product Image"
+              />
+            </picture>
+          </AppButton>
+
+          <!-- Controls -->
+          <div class="absolute inset-0 grid grid-flow-col justify-between items-center">
+            <IconPrevButton @click="prevItem" class="transform -translate-x-1/2" />
+            <IconNextButton @click="nextItem" class="transform translate-x-1/2" />
+          </div>
+        </div>
 
         <ul class="w-[85%] mx-auto grid grid-cols-4 gap-4 relative">
-          <li v-for="(img, idx) in imgList" :key="`product-${idx}`">
-            <AppButton variant="flat" class="rounded-lg overflow-hidden w-full h-full">
+          <li v-for="(img, idx) in imgList" :key="`product-${idx}`" @click="changeMainImg(img)">
+            <AppButton variant="flat" class="rounded-lg overflow-hidden w-full h-full group">
               <img
                 :src="`/img/products/${img}-thumbnail.jpg`"
-                class="w-full h-full object-cover"
+                class="w-full h-full object-cover transform group-focus:scale-120 duration-300"
                 width="73"
                 height="79"
                 :alt="`Product thumbnail #${idx}`"
@@ -52,7 +96,10 @@ const { imgList, mainImg } = store.product;
             </AppButton>
           </li>
           <li class="absolute inset-0 grid grid-cols-4 gap-4 pointer-events-none">
-            <div class="border-2 border-primary-800 rounded-lg bg-white/50 duration-200"></div>
+            <div
+              class="border-2 border-primary-800 rounded-lg bg-white/50 duration-200"
+              ref="activeThumbnail"
+            ></div>
           </li>
         </ul>
       </div>

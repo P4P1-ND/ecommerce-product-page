bdc7d5630606299d968f58345d62450bc625e9f6
Papy NDOYE <ndoyepapy@gmail.com>
2021-12-12 09:40:25 +0000

    feat(access): add h1 tag for screen readers

--- src/components/organisms/AppProduct.vue
+++ src/components/organisms/AppProduct.vue
@@ -5,6 +5,7 @@ import AppProductShowcase from "../molecules/AppProductShowcase.vue";
 
 <template>
   <main class="grid lg:(mt-20 px-10 grid-cols-[45%,40%] items-center justify-between)">
+    <h1 class="sr-only">Product Page</h1>
     <AppProductShowcase />
     <AppProductInfo />
   </main>
